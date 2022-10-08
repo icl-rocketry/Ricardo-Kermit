762a34bf4de89387e744e2fb56404dde8f4e1c87
kiran <kd619@ic.ac.uk>
2022-10-08 01:20:31 +0100

    andrei use initializer lists

--- Sensors/Software/src/NTCThermistor/NTCthermistor.cpp
+++ Sensors/Software/src/NTCThermistor/NTCthermistor.cpp
@@ -2,37 +2,42 @@
 #include <ADS1219/ADS1219.h>
 #include <global_config.h>
 
-NTCThermistor::NTCThermistor(ADS1219* ADS, const uint32_t Rfixed, const uint32_t Rseriesextra, const float A, const float B, const float C, uint8_t ADCchannel):
-_ADS(ADS)
+NTCThermistor::NTCThermistor(ADS1219 *ADS, const uint32_t Rfixed, const uint32_t Rseriesextra, const float A, const float B, const float C, uint8_t ADCchannel) : 
+_ADS(ADS),
+SHHA(A),
+SHHB(B),
+SHHC(C),
+RFixedPD(Rfixed),
+Rseries(Rseriesextra),
+channel(ADCchannel)
 {
-SHHA = A;
-SHHB = B;
-SHHC = C;
-RFixedPD = Rfixed;
-Rseries = Rseriesextra;
-channel = ADCchannel;
+    // SHHA = A;
+    // SHHB = B;
+    // SHHC = C;
+    // RFixedPD = Rfixed;
+    // Rseries = Rseriesextra;
+    // channel = ADCchannel;
 };
 
-NTCThermistor::NTCThermistor(ADS1219* ADS, const uint32_t Rfixed, const uint32_t Rseriesextra, const float gradient, const float Cconstant, uint8_t ADCchannel):
-_ADS(ADS)
+NTCThermistor::NTCThermistor(ADS1219 *ADS, const uint32_t Rfixed, const uint32_t Rseriesextra, const float gradient, const float Cconstant, uint8_t ADCchannel) : _ADS(ADS)
 {
-grad = gradient;
-constant = Cconstant;
-RFixedPD = Rfixed;
-Rseries = Rseriesextra;
-channel = ADCchannel;
+    grad = gradient;
+    constant = Cconstant;
+    RFixedPD = Rfixed;
+    Rseries = Rseriesextra;
+    channel = ADCchannel;
 };
 
 float NTCThermistor::getTemp()
 {
-    Rtherm = RFixedPD * ((16777215/_ADS->readAdjusted(channel))-1) - Rseries;
-    Temp = 1 / (SHHA + SHHB * log(Rtherm) + SHHC * pow(log(Rtherm),3));
+    Rtherm = RFixedPD * ((16777215 / _ADS->readAdjusted(channel)) - 1) - Rseries;
+    Temp = 1 / (SHHA + SHHB * log(Rtherm) + SHHC * pow(log(Rtherm), 3));
     return Temp;
 }
 
-float NTCThermistor::getTempLinear(){
-    Rtherm = RFixedPD * ((16777215/_ADS->readAdjusted(channel))-1) - Rseries;
+float NTCThermistor::getTempLinear()
+{
+    Rtherm = RFixedPD * ((16777215 / _ADS->readAdjusted(channel)) - 1) - Rseries;
     Temp = grad * Rtherm + constant;
     return Temp;
-
 }
