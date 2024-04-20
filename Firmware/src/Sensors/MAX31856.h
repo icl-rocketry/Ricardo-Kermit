882a501322fcd96d06a3a76876fa974230c8f885
AndreiP <paduraru.andrei5@gmail.com>
2024-04-20 03:02:35 +0100

    Added fault checking and an alive method to the MAX31856

--- Firmware/src/Sensors/MAX31856.h
+++ Firmware/src/Sensors/MAX31856.h
@@ -16,31 +16,44 @@
 
 class MAX31856{
     
+    private: //This is only here because arduino requires enums to be defined before they're used as a function input.
+        enum Faults : uint8_t {
+            CJRange = (1<<7),
+            TCRange = (1<<6),
+            CJHigh = (1<<5),
+            CJLow = (1<<4),
+            TCHigh = (1<<3),
+            TCLow = (1<<2),
+            OVUV = (1<<1),
+            OPEN = (1<<0),
+        };
+
     public:
+        //Enums to make device settings readable.
         enum TCType : uint8_t {
-            TB = 0b0000,
-            TE = 0b0001,
-            TJ = 0b0010,
-            TK = 0b0011,
-            TN = 0b0100,
-            TR = 0b0101,
-            TS = 0b0110,
-            TT = 0b0111
+            TB = 0b00000000,
+            TE = 0b00000001,
+            TJ = 0b00000010,
+            TK = 0b00000011,
+            TN = 0b00000100,
+            TR = 0b00000101,
+            TS = 0b00000110,
+            TT = 0b00000111
         };
 
         enum OCSet : uint8_t {
-            disabled = 0b00,
-            RSL5K = 0b01,
-            RSH5KTCL2 = 0b10,
-            RSH5KTCH2 = 0b11
+            disabled = 0,
+            RSL5K = 0b00010000,
+            RSH5KTCL2 = 0b00100000,
+            RSH5KTCH2 = 0b00110000
         };
         
         enum AVSet : uint8_t {
-            onesamp = 0b000,
-            twosamp = 0b001,
-            foursamp = 0b010,
-            eightsamp = 0b011,
-            sixteensamp = 0b100
+            onesamp = 0,
+            twosamp = 0b00010000,
+            foursamp = 0b00100000,
+            eightsamp = 0b00110000,
+            sixteensamp = 0b01000000
         };
 
         enum ConvModes : uint8_t {
@@ -63,9 +76,9 @@ class MAX31856{
             fiftyHz = 0
         };
         
-        MAX31856(SPIClass &spi, uint8_t cs, TCType TCType = TCType::TK, OCSet OCSet = OCSet::RSH5KTCL2, AVSet AVSet = AVSet::onesamp, ConvModes Mode = ConvModes::conti, ENCJComp EN = ENCJComp::disable, FaultModes Fault = FaultModes::comparator, FilterFreqs Freq = FilterFreqs::sixtyHz);
-        void setup();
-        void reset();
+        MAX31856(SPIClass &spi, uint8_t cs, int8_t faultPin = -1, int8_t DRDYPin = -1);
+        bool setup(TCType TCType = TCType::TK, OCSet OCSet = OCSet::RSL5K, AVSet AVSet = AVSet::onesamp, ConvModes Mode = ConvModes::conti, ENCJComp EN = ENCJComp::enable, FaultModes Fault = FaultModes::comparator, FilterFreqs Freq = FilterFreqs::sixtyHz); //TODO modify to take in a struct and return a bool for whether setup was successful.
+        void reset(); //TODO implement
         void setTCType(TCType TCType);
         void setOCDetection(OCSet OCSet);
         void setAveraging(AVSet AVset);
@@ -74,7 +87,19 @@ class MAX31856{
         void setFaultMode(FaultModes Mode);
         void setFilter(FilterFreqs Freq);
         void setCJOffset(float CJT);
-        float getTemp() {return Temp;}
+        void maskFault(Faults FaultToMask); //TODO implement
+        float getTemp() {return m_Temp;};
+        /**
+         * @brief Getter for device fault flags that are raised.
+         * @return Faults encoded in a bitfield, which is described by the "Fault" enum.
+        */
+        uint8_t getFault() {return m_activeFaults;};
+        /**
+         * @brief Checks if "Fault" is raised.
+         * @return True if the fault is raised, false otherwise.
+        */
+        bool checkFault(Faults Fault);
+
         void update();
         
     private:
@@ -112,35 +137,28 @@ class MAX31856{
             CJTH = 0x8A,
             CJTL = 0x8B,
         };
-
-        enum class Faults : uint8_t {
-            CJRange,
-            TCRange,
-            CJHigh,
-            CJLow,
-            TCHigh,
-            TCLow,
-            OVUV,
-            OPEN,
-        };
         
         uint32_t readRegister(readRegisters target, uint8_t Nbytes);
         void writeRegister(writeRegisters target, uint8_t data);
-        void setParameter(writeRegisters RegisterAddr, uint8_t Register, uint8_t Mask, uint8_t Parameter);
+        void setParameter(writeRegisters RegisterAddr, uint8_t& ShadowRegister, uint8_t Mask, uint8_t Parameter);
+        /**
+         * @brief Identify if the device is alive. Note, this device doesn't have a whoami register, so this is only implemented as check if any registers are non-zero, which should be the case because some registers reset to a non-zero value.
+         * @return True if the device is communicating, false otherwise.
+        */
+        bool alive();
         void clearFault();
 
-
-
         //Initialising
-        float Temp = 0;
+        float m_Temp = 0;
         uint32_t m_prevUpdate = 0;
         uint16_t m_updateDelta = 41;
+        bool m_init = false;
+        Faults m_activeFaults = static_cast<Faults>(0); //Initialised to 0, i.e. no faults active.
 
-        //SPI
-        SPIClass& _spi;
-        SPISettings _spisettings;
-
-        uint8_t _cs;
+        //SPI adjacent settings
+        SPIClass& m_spi;
+        SPISettings m_spisettings;
+        uint8_t m_cs;
 
         //Settings
         TCType _Type;
@@ -155,13 +173,15 @@ class MAX31856{
         uint8_t C0Reg = 0x00;
         uint8_t C1Reg = 0x03;
         uint8_t CJOffReg = 0x00;
+
+        //Magic numbers
+        static constexpr float fpScale = 1.0/4096.0; //floating point conversion scale to convert a temperature stored in binary to a float.
     
-        //Masks
-        static constexpr float fpScale = 1.0/4096.0;
+        //General masks
         static constexpr uint32_t SignMask24Bit = 0b100000000000000000000000;
         static constexpr uint32_t MaskAll24Bits = 0b111111111111111111111111;
         
-        //C0
+        //Masks for CONFIG0 register options
         static constexpr uint8_t ModeMask = 0b01111111;
         static constexpr uint8_t OCFaultMask = 0b11001111;
         static constexpr uint8_t CJCompENMask = 0b11110111;
@@ -169,7 +189,7 @@ class MAX31856{
         static constexpr uint8_t FaultClearMask = 0b11111101;
         static constexpr uint8_t FilterMask = 0b11111110;
 
-        //C1
+        //Masks for CONFIG1 register options
         static constexpr uint8_t TypeMask = 0b11110000;
         static constexpr uint8_t AVMask = 0b10001111;
 
